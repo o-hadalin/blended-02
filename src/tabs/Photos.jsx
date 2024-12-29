e8925d5e480f0a97b350c6bd12b41fe3be17362a
oksana-rybalka <rybalka-oksana@outlook.com>
2024-12-29 13:02:34 +0200

    Photos

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,11 +1,31 @@
-// import { getPhotos } from 'apiService/photos';
-
+import { getPhotos } from '../apiService/photos';
+import Form from '../components/Form/Form';
 import Text from '../components/Text/Text';
+import { useEffect, useState } from 'react';
 
 const Photos = () => {
+  const [query, setQuery] = useState('');
+  const onSubmit = text => {
+    setQuery(text);
+  };
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    async function fetchFotos() {
+      try {
+        const photos = await getPhotos(query, 1);
+        console.log(photos);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchFotos();
+  }, [query]);
   return (
     <>
-      <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={onSubmit} />
     </>
   );
 };
